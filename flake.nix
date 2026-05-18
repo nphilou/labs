20339b08b2457ea3345f400f3abbf8df95d61bab
philippe <nphilou@gmail.com>
2026-05-18 18:25:05 +0200

    Add hello lab app

--- flake.nix
+++ flake.nix
@@ -5,7 +5,27 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { ... }: {
-    nixosModules.default = import ./nixos/module.nix;
-  };
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      hello = pkgs.stdenvNoCC.mkDerivation {
+        pname = "labs-hello";
+        version = "0.1.0";
+        src = ./apps/hello;
+
+        installPhase = ''
+          runHook preInstall
+          mkdir -p $out
+          cp -r . $out/
+          runHook postInstall
+        '';
+      };
+    in
+    {
+      packages.${system}.hello = hello;
+      packages.${system}.default = hello;
+
+      nixosModules.default = import ./nixos/module.nix;
+    };
 }
--- nixos/module.nix
+++ nixos/module.nix
@@ -1,9 +1,48 @@
-{ lib, ... }:
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.nphilou.labs;
+  hello = pkgs.stdenvNoCC.mkDerivation {
+    pname = "labs-hello";
+    version = "0.1.0";
+    src = ../apps/hello;
+
+    installPhase = ''
+      runHook preInstall
+      mkdir -p $out
+      cp -r . $out/
+      runHook postInstall
+    '';
+  };
+in
 
 {
   options.nphilou.labs = {
     enable = lib.mkEnableOption "nphilou labs app platform";
   };
 
-  config = {};
+  config = lib.mkIf cfg.enable {
+    systemd.services.labs-hello = {
+      description = "Labs hello app";
+      after = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+
+      serviceConfig = {
+        DynamicUser = true;
+        ExecStart = "${pkgs.python3}/bin/python -m http.server 9101 --bind 127.0.0.1 --directory ${hello}";
+        Restart = "always";
+        RestartSec = "5s";
+      };
+    };
+
+    services.nginx.virtualHosts."app.nphilou.ch".locations = {
+      "/hello" = {
+        return = "301 /hello/";
+      };
+
+      "/hello/" = {
+        proxyPass = "http://127.0.0.1:9101/";
+      };
+    };
+  };
 }
