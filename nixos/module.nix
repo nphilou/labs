a444fe7a311db7e9dfae5098ebfc37554e7268aa
Philippe <nphilou@gmail.com>
2026-05-19 10:54:24 +0200

    Refactor hello app into dedicated NixOS module

--- nixos/module.nix
+++ nixos/module.nix
@@ -2,47 +2,22 @@
 
 let
   cfg = config.nphilou.labs;
-  hello = pkgs.stdenvNoCC.mkDerivation {
-    pname = "labs-hello";
-    version = "0.1.0";
-    src = ../apps/hello;
-
-    installPhase = ''
-      runHook preInstall
-      mkdir -p $out
-      cp -r . $out/
-      runHook postInstall
-    '';
-  };
+  sendDeployEmail = pkgs.writers.writePython3Bin "labs-send-deploy-email" {
+    libraries = with pkgs.python3Packages; [ resend python-dotenv ];
+    flakeIgnore = [ "E501" ];
+  } (builtins.readFile ../scripts_send_deploy_email.py);
 in
 
 {
+  imports = [
+    ./apps/hello.nix
+  ];
+
   options.nphilou.labs = {
     enable = lib.mkEnableOption "nphilou labs app platform";
   };
 
   config = lib.mkIf cfg.enable {
-    systemd.services.labs-hello = {
-      description = "Labs hello app";
-      after = [ "network.target" ];
-      wantedBy = [ "multi-user.target" ];
-
-      serviceConfig = {
-        DynamicUser = true;
-        ExecStart = "${pkgs.python3}/bin/python -m http.server 9101 --bind 127.0.0.1 --directory ${hello}";
-        Restart = "always";
-        RestartSec = "5s";
-      };
-    };
-
-    services.nginx.virtualHosts."app.nphilou.ch".locations = {
-      "/hello" = {
-        return = "301 /hello/";
-      };
-
-      "/hello/" = {
-        proxyPass = "http://127.0.0.1:9101/";
-      };
-    };
+    environment.systemPackages = [ sendDeployEmail ];
   };
 }
